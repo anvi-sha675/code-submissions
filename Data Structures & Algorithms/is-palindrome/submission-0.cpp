e58d3cbbca77f8a17b487662c40731fc6b5942b1
Anvesha <anvi5765sha@gmail.com>
2026-06-29 18:04:36 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str="";
+       for(int i=0;i<s.length();i++){
+        if(isalnum(s[i])) str+=static_cast<char>(tolower(s[i]));
+       }
+       int i=0, j=str.length()-1;
+       while(i<j){
+        if(str[i]!=str[j]) return false;
+        i++;
+        j--;
+       }
+       return true;
+    }
+};
