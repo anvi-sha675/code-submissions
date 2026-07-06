f155cb07a869e6890a8d3b320a4ff48d99d5ffe0
Anvesha <anvi5765sha@gmail.com>
2026-07-06 23:41:17 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int>dp;
+    int solve(int n){
+        if(n<3) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solve(n-1)+solve(n-2);
+    }
+    int climbStairs(int n) {
+        dp.resize(n+1, -1);
+        return solve(n);
+    }
+};
